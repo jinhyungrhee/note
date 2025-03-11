40ad14d46e659b82d803e798c024786fdf406fc4
jinhyungrhee <rheejinhyung@gmail.com>
2025-03-11 10:36:35 +0900

    [알고리즘인터뷰]6장_문제05_그룹_애너그램

---
+++ src/algo/interview/a_string/group_anagrams/GroupAnagrams01.java
@@ -0,0 +1,36 @@
+package algo.interview.a_string.group_anagrams;
+
+import java.util.*;
+public class GroupAnagrams01 {
+    public static void main(String[] args) {
+        String[] words = {"eat", "tea", "tan", "ate", "ant", "cat"};
+        List<List<String>> result = groupAnagrams(words);
+        System.out.println(result);
+
+    }
+
+    private static List<List<String>> groupAnagrams(String[] words) {
+
+
+        HashMap<String, List<String>> result = new HashMap();
+        for (String word : words) {
+            char[] chars = word.toCharArray();
+
+            // 1.문자열 정렬
+            Arrays.sort(chars);
+            //System.out.println(chars);
+
+            // 2.정렬된 문자를 맵의 key로 사용
+            String key = String.valueOf(chars);
+            if (!result.containsKey(key)) {
+                result.put(key, new ArrayList<>());
+            }
+            result.get(key).add(word);
+        }
+        //System.out.println(result);
+
+        List<List<String>> answer = new ArrayList<>(result.values());
+        //System.out.println(answer);
+        return answer;
+    }
+}
