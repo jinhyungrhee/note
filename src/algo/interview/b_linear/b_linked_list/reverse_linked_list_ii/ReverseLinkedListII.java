65b629f5fa2e5a2e93a9530d7e521bb01e027cb8
jinhyungrhee <rheejinhyung@gmail.com>
2025-04-01 15:14:04 +0900

    [알고리즘인터뷰]8장_문제19_역순_연결_리스트_2

--- src/algo/interview/b_linear/b_linked_list/reverse_linked_list_ii/ReverseLinkedListII.java
+++ src/algo/interview/b_linear/b_linked_list/reverse_linked_list_ii/ReverseLinkedListII.java
@@ -10,9 +10,25 @@ public class ReverseLinkedListII {
 
     public static void main(String[] args) {
 
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        int left = 3;
+        int right = 5;
+
+        ListNode result = reverseBetween(head, left, right);
+        while(result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+
     }
 
-    public ListNode reverseBetween(ListNode head, int left, int right) {
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
         if (head == null) return null;
 
         ListNode root = new ListNode(0);
