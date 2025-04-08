d41eacffce6da730c2710667de34360edb0a96b4
jinhyungrhee <rheejinhyung@gmail.com>
2025-04-08 10:23:35 +0900

    add solution of valid-anagram

--- src/algo/interview/z_leetcode/ValidAnagram242.java
+++ src/algo/interview/z_leetcode/ValidAnagram242.java
@@ -83,23 +83,23 @@ public class ValidAnagram242 {
      *
      */
 
-//    public static boolean validAnagram02(String s, String t) {
-//
-//        if (s.length() != t.length()) return false;
-//
-//        Map<Character, Integer> sFrequency = new HashMap<>();
-//        Map<Character, Integer> tFrequency = new HashMap<>();
-//
-//        for (int i = 0; i < s.length(); i++) {
-//            sFrequency.put(s.charAt(i), sFrequency.getOrDefault(s.charAt(i), 0) + 1);
-//            tFrequency.put(t.charAt(i), tFrequency.getOrDefault(t.charAt(i), 0) + 1);
-//        }
-//
-//        for (int i = 0; i < s.length(); i++) {
-//            if (!sFrequency.get(s.charAt(i)).equals(tFrequency.get(s.charAt(i)))) return false;
-//        }
-//        return true;
-//    }
+    public static boolean validAnagram02(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> sFrequency = new HashMap<>();
+        Map<Character, Integer> tFrequency = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            sFrequency.put(s.charAt(i), sFrequency.getOrDefault(s.charAt(i), 0) + 1);
+            tFrequency.put(t.charAt(i), tFrequency.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!sFrequency.get(s.charAt(i)).equals(tFrequency.get(s.charAt(i)))) return false;
+        }
+        return true;
+    }
 
 
     /**
