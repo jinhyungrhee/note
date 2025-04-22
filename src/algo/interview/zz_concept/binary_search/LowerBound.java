d39271b592dd639038f12ce5cf07a4607fb7a85f
jinhyungrhee <rheejinhyung@gmail.com>
2025-04-22 15:42:46 +0900

    concept of LowerBound & UpperBound (binary-search)

--- src/algo/interview/zz_concept/binary_search/LowerBound.java
+++ src/algo/interview/zz_concept/binary_search/LowerBound.java
@@ -1,15 +1,17 @@
 package algo.interview.zz_concept.binary_search;
 
 public class LowerBound {
-    static int arr[] = {1, 3, 5, 7, 8, 10, 20, 35, 35, 35, 99, 100};
+    static int arr[] = {1, 2, 2, 3, 3, 3, 5, 6, 8};
 
     public static void main(String[] args) {
-        System.out.println(lowerBound(35));
+        System.out.println(lowerBound(3));
+        System.out.println(lowerBound2(3));
     }
 
     /**
      * Lower Bound
-     * arr[i] ≥ x를 만족하는 가장 작은 i
+     * arr[i] ≥ x를 만족하는 가장 작은 i (-> 이상)
+     * 즉, 특정 값의 '시작위치'를 찾는 알고리즘
      */
     public static int lowerBound(int target) {
         int left = 0, right = arr.length;
@@ -22,5 +24,21 @@ public class LowerBound {
             }
         }
         return left; // 가장 첫번째 위치이므로 left 리턴
+    }
+
+    /**
+     *  '이상'의 의미를 더 잘 나타내기 위한 리팩토링
+     */
+    public static int lowerBound2(int target) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] >= target) { // 중간값이 찾는값 보다 크거나 같으면 왼쪽 탐색
+                right = mid;
+            } else { // (arr[mid] < target) 중간값이 찾는값 보다 작으면 오른쪽 탐색
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
--- src/algo/interview/zz_concept/binary_search/UpperBound.java
+++ src/algo/interview/zz_concept/binary_search/UpperBound.java
@@ -1,15 +1,17 @@
 package algo.interview.zz_concept.binary_search;
 
 public class UpperBound {
-    static int arr[] = {1, 3, 5, 7, 8, 10, 20, 35, 35, 35, 99, 100};
+    static int arr[] = {1, 2, 2, 3, 3, 3, 5, 6, 8};
 
     public static void main(String[] args) {
-        System.out.println(upperBound(35));
+        System.out.println(upperBound(3));
+        System.out.println(upperBound2(3));
     }
 
     /**
      * Upper Bound
-     * arr[i] > x 를 만족하는 가장 작은 i
+     * arr[i] > x 를 만족하는 가장 작은 i (-> 초과)
+     *  즉, 특정 값 k보다 '처음으로 큰 값의 위치'를 찾는 알고리즘
      */
     public static int upperBound(int target) {
         int left = 0, right = arr.length;
@@ -17,12 +19,26 @@ public class UpperBound {
             int mid = (left + right) / 2;
             if (arr[mid] <= target) {
                 left = mid + 1;
-            }
-            else {
+            } else {
                 right = mid;
             }
         }
         return left;
     }
 
+    /**
+     * 초과'의 의미를 더 잘 나타내기 위한 리팩토링
+     */
+    public static int upperBound2(int target) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > target) { // 중간값이 찾는값 보다 크면 왼쪽 탐색
+                right = mid;
+            } else { // (arr[mid] <= target) 중간값이 찾는값 보다 작거나 같으면 오른쪽 탐색
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 }
